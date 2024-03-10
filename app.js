4a4a5f8b3ff11e90bd8c561a63f9f2bd91e71449
rifatbinmoshiur2004 <tfarinnosseh200@gmail.com>
2024-03-11 03:40:57 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.add').click(function(){
+
+        
+        $('.field').append('<div><input> <a href="#" class="remove_field">remove_field</a></div>')
+
+
+
+
+
+
+
+       
+    })
+});
+
+$('.field').on("click", ".remove_field", function(){
+    $(this).parent('div').remove()
+})
+
+
+
